21f3598a8b4495cdb45cc85a75ff557a02b186e6
Pikuman83 <wahab_anjum@hotmail.com>
2022-02-05 14:05:42 +0100

    router-test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvitadoComponent } from './components/invitado/invitado.component';
+import { VenueComponent } from './components/venue/venue.component';
 
 const routes: Routes = [
-  {path: 'invitado/:invitado', component: InvitadoComponent }
+  {path: '', redirectTo: '/venue', pathMatch: 'full'},
+  {path: 'invitado/:invitado', component: InvitadoComponent },
+  {path: 'venue', component: VenueComponent }
 ];
 
 @NgModule({
