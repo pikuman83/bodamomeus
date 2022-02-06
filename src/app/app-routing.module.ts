484ea1d1b4893d7835bce060259941abc954f304
Pikuman83 <wahab_anjum@hotmail.com>
2022-02-06 18:48:08 +0100

    h

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InvitadoComponent } from './components/invitado/invitado.component';
+import { VenueComponent } from './components/venue/venue.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/invitado/o-Ali', pathMatch: 'full'},
+  {path: 'venue', component: VenueComponent },
   {path: 'invitado/:id', component: InvitadoComponent },
-  {path: 'dashboardRM', component: DashboardComponent }
+  // {path: 'dashboardRM', component: DashboardComponent }
 ];
 
 @NgModule({
